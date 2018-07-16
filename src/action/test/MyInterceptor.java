814ccc46ccc826fd25a070733e9f9622c5905487
SuksawatTawana <suksawat.taw@northbkk.ac.th>
2018-07-16 17:03:15 +0700

    update

--- src/action/test/MyInterceptor.java
+++ src/action/test/MyInterceptor.java
@@ -8,13 +8,14 @@ public class MyInterceptor extends AbstractInterceptor {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	public String intercept(ActionInvocation invocation) throws Exception {
+	public String intercept(ActionInvocation actionInvocation) throws Exception {
 		
 		String output = "Pre-Processing"; 
 	    System.out.println(output);
 	      
 	    /* let us call action or next interceptor */
-	    String result = invocation.invoke();
+	    String result = actionInvocation.invoke();
+	    System.out.println("result --> " + result);
 
 	    /* let us do some post-processing */
 	    output = "Post-Processing"; 
